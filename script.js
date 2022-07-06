3978b1a28c1ea723610809a72583fc83a1272ba3
mihalis1 <103450474+mihalis1@users.noreply.github.com>
2022-07-06 13:49:10 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,3 @@
-// //force https
-
-// if (location.protocol !== 'https:') {
-//     location.replace(`https:${location.href.substring(location.protocol.length)}`);
-// }
-
-let header = document.querySelector('header')
-let windowY
-
-document.addEventListener('scroll', (a) => {
-    windowY = window.scrollY
-    if(windowY == 0){
-
-    }
-    console.log(windowY)
-})
-
-
+if (location.protocol !== 'https:') {
+     location.replace(`https:${location.href.substring(location.protocol.length)}`);
+}
